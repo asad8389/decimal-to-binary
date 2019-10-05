755664145553334aeeac30df262f1ac7cd972335
Asad Ali Khan <48471395+asad8389@users.noreply.github.com>
2019-10-06 00:51:38 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int binary(int);
+int main()
+{
+    int n;
+
+    printf("enter a number:\n");
+    scanf("%d",&n);
+
+    printf("binary is %d \n",binary(n));
+
+
+}
+int binary(int x)
+{
+    int a,b;
+
+    if(x==0)
+    {
+        return 0;
+    }
+    else
+    {
+        a=x%2;
+        return(a+10*binary(x/2));
+    }
+}
